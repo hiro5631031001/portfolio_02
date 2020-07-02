9429a48fd69db2829b3719a4c2bd80ffa9661b0b
hiro5631031001 <hiro5631031001@gmail.com>
2020-07-03 06:23:10 +0900

    Update books_controller.rb
    newメソッドを作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,36 @@
 class BooksController < ApplicationController
   def index
   end
+
+  def new
+    @books = []
+
+    @keyword = params[:keyword]
+    if @keyword.present?
+      apibooks = RakutenWebService::Books::Book.search({title: @keyword, hit: 15})
+
+      apibooks.each do |apibook|
+        book = Book.new(change(apibook))
+        @books = book
+      end
+    end
+  end
+
+  private 
+
+  def change(apibook)
+    title = apibook['title']
+    author = apibook['author']
+    publisher = apibook['publisherName']
+    isbn = apibook['isbn']
+    image = apibook['mediumImageUrl']
+
+    {
+      title: title,
+      isbn: isbn,
+      publisher: publisher,
+      image: image 
+    }
+  end
+
 end
