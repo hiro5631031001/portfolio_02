0fb28282e391b441fc57ae69d0e4928936c13991
hiro5631031001 <hiro5631031001@gmail.com>
2020-07-03 05:52:17 +0900

    Create rakuten.rb
    楽天API使用のためのファイル作成

---
+++ config/initializers/rakuten.rb
@@ -0,0 +1,8 @@
+RakutenWebService.configure do |c|
+  # (必須) アプリケーションID
+  c.application_id = '1016884356305082372'
+
+  # (任意) 楽天アフィリエイトID
+  c.affiliate_id = '1bce16e7.5ec2f2df.1bce16e8.ea9da334'
+
+end
