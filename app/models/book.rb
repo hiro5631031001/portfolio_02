ad07ff5f45952abe3c49c121faa05f632beb58e8
hiro5631031001 <hiro5631031001@gmail.com>
2020-07-05 10:19:18 +0900

    Update book.rb

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,10 @@
 class Book < ApplicationRecord
+
+  validates :title,      presence: true
+  validates :author,     presence: true
+  validates :publisher,  presence: true
+  validates :isbn,       presence: true
+  validates :image,      presence: true
+  validates :url,        presence: true
+
 end
