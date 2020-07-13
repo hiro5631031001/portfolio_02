a92fed68062611956cdb02f2210fde63be8ff866
hiro5631031001 <hiro5631031001@gmail.com>
2020-07-13 23:48:24 +0900

    Update comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,14 @@
 class CommentsController < ApplicationController
 
   def create
-    Comment.create(comment_params)
+    comment = Comment.create(comment_params)
+    redirect_to root_path
   end
 
   private
 
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, book_id: params[:book_id])
+  end
 
 end
