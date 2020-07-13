aed2bbb8416e1d3bea5f81ad166bb0dbcc1e776d
hiro5631031001 <hiro5631031001@gmail.com>
2020-07-13 23:36:55 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "books#index"
-  resources :books , only: [:index, :new, :create, :show, :destroy]
+  resources :books , only: [:index, :new, :create, :show, :destroy] do
+    resources :comments
+  end
 end
